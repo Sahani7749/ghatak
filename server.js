27b3e36ac9d4c86bb3cb27b497230a4926a1f7b7
Sahani7749 <chandrakantasahani4@gmail.com>
2025-09-06 17:06:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -147,15 +147,9 @@ admin.initializeApp({
 
 
 
-(async () => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-  });
-  const page = await browser.newPage();
-  await page.goto("https://google.com");
-  console.log(await page.title());
-  await browser.close();
-})();
+const browser = await puppeteer.launch({
+  args: ["--no-sandbox", "--disable-setuid-sandbox"]
+});
 
 
 
